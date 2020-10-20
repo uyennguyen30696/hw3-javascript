7d4d86e619e2770ef5747755f0c1064cf3f94adc
Uyen Nguyen <udnguyen@ucsd.edu>
2020-10-20 15:54:13 -0700

    clean up version 1

--- version-01/script1.js
+++ version-01/script1.js
@@ -11,71 +11,69 @@ var criteria = [ ];
 
 // Choose password length
 var passLength = parseInt(prompt("Choose your password length from 8 to 128 characters"));
-console.log(passLength);
+
+// Make sure user enter the passLength from 8 to 128 characters
+if (passLength < 8 || passLength > 128) {
+    alert("Please enter your password length between 8 to 128 characters!");
+}
+else {
+    console.log("Password length = " + passLength);
+}
 
 // Confirm the criteria options
 var lowerCase = confirm("Include lower case letters?");
-console.log(lowerCase);
+console.log("Include lower case: " + lowerCase);
 
 var upperCase = confirm("Include upper case letters?");
-console.log(upperCase);
+console.log("Include upper case: " + upperCase);
 
 var number = confirm("Include numbers?");
-console.log(number);
+console.log("Include number: " + number);
 
 var specialChar = confirm("Include special characters?");
-console.log(specialChar);
+console.log("Include special characters: " + specialChar);
 
 var password = [];
 
 // Create function to generate password by selected criteria
 function genPass() {
-    
+    // Push each of the criteria in the criteria array
     if (lowerCase === true) {
-        // criteria.push(lowerCaseArray);
+         criteria.push(lowerCaseArray);
         for(let j = 0; j< lowerCaseArray.length; j++){
             criteria.push(lowerCaseArray[j])
         }
     }
 
     if (upperCase === true) {
-        // criteria.push(upperCaseArray);
+         criteria.push(upperCaseArray);
         for(let j = 0; j< upperCaseArray.length; j++){
             criteria.push(upperCaseArray[j])
             }
         }
 
     if (number === true) {
-        // criteria.push(numberArray);
+         criteria.push(numberArray);
         for(let j = 0; j< numberArray.length; j++){
             criteria.push(numberArray[j])
             }
         }
 
     if (specialChar === true) {
-        // criteria.push(specialCharArray);
+         criteria.push(specialCharArray);
         for(let j = 0; j< specialCharArray.length; j++){
             criteria.push(specialCharArray[j])
             }
         }
 }
-var passwordLength = passLength + 1;
 
-console.log("password",password);
-
-// Call function
 genPass();
 console.log(criteria);
-console.log(passLength);
 
-for (var j = 0; j < passwordLength -1; j++) {
-    var random = Math.floor(Math.random() * criteria.length);
-    console.log(random);
-    password.push(criteria[random]);
+for (var i = 0; i < passLength; i++) {
+    var randomCriteria = Math.floor(Math.random() * criteria.length);
+    // Randomly pick from criteria array that already includes all criteria
+    password.push(criteria[randomCriteria]);
 }
 
-console.log(password.toString().replace(/,/g, ''));
-
-// Add generated password to display box
-// document.getElementById("display".value = password);
-// document.addEventListener("click"), generatePassword;
+console.log("Password: " + password.toString().replace(/,/g, ''));
