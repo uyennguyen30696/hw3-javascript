f55e930772c583ad0269f7fd069451d0bfb17eae
Uyen Nguyen <udnguyen@ucsd.edu>
2020-10-31 18:47:24 -0700

    update version 1

--- version-01/script1.js
+++ version-01/script1.js
@@ -4,76 +4,131 @@
 // Array of characters
 var lowerCaseArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var upperCaseArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-var numberArray = ["0","1","2","3","4","5","6","7","8","9"];
+var numberArray = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var specialCharArray = ["~", "!", "@", "#", "$", "%", "&", "*", ".", "/", ":", ";", "<", "=", ">", "?", "_", "{", "[", "}", "]", "|"];
 
-var criteria = [ ];
+// Function for user input
+function userInput() {
 
-// Choose password length
-var passLength = parseInt(prompt("Choose your password length from 8 to 128 characters"));
+    // Choose password length
+    var passLength = parseInt(
+        prompt("Choose your password length from 8 to 128 characters")
+    );
+    console.log(passLength);
 
-// Make sure user enter the passLength from 8 to 128 characters
-if (passLength < 8 || passLength > 128) {
-    alert("Please enter your password length between 8 to 128 characters!");
-}
-else {
-    console.log("Password length = " + passLength);
-}
-
-// Confirm the criteria options
-var lowerCase = confirm("Include lower case letters?");
-console.log("Include lower case: " + lowerCase);
-
-var upperCase = confirm("Include upper case letters?");
-console.log("Include upper case: " + upperCase);
-
-var number = confirm("Include numbers?");
-console.log("Include number: " + number);
-
-var specialChar = confirm("Include special characters?");
-console.log("Include special characters: " + specialChar);
-
-var password = [];
-
-// Create function to generate password by selected criteria
-function genPass() {
-    // Push each of the criteria in the criteria array
-    if (lowerCase === true) {
-         criteria.push(lowerCaseArray);
-        for(let j = 0; j< lowerCaseArray.length; j++){
-            criteria.push(lowerCaseArray[j])
-        }
+    // Conditional check if passLength is a number
+    if (isNaN(passLength) === true) {
+        alert("Password length must be a number.");
+        return;
     }
 
-    if (upperCase === true) {
-         criteria.push(upperCaseArray);
-        for(let j = 0; j< upperCaseArray.length; j++){
-            criteria.push(upperCaseArray[j])
-            }
+    if (passLength < 8 || passLength > 128) {
+        alert("Please enter your password length between 8 to 128 characters!");
+        return;
+    }
+
+    // Confirm the criteria options
+    var lowerCase = confirm("Include lower case letters?");
+    console.log("Include lower case: " + lowerCase);
+
+    var upperCase = confirm("Include upper case letters?");
+    console.log("Include upper case: " + upperCase);
+
+    var number = confirm("Include numbers?");
+    console.log("Include number: " + number);
+
+    var specialChar = confirm("Include special characters?");
+    console.log("Include special characters: " + specialChar);
+
+    // If none of the criteria is chosen
+    if (lowerCase === false &&
+        upperCase === false &&
+        number === false &&
+        specialChar === false) {
+            alert("Password must contain at least one character type.");
+            return;
         }
 
-    if (number === true) {
-         criteria.push(numberArray);
-        for(let j = 0; j< numberArray.length; j++){
-            criteria.push(numberArray[j])
-            }
-        }
-
-    if (specialChar === true) {
-         criteria.push(specialCharArray);
-        for(let j = 0; j< specialCharArray.length; j++){
-            criteria.push(specialCharArray[j])
-            }
-        }
+    // Create an object to store user input
+    var chosenChar = {
+        passLength: passLength,
+        lowerCase: lowerCase,
+        upperCase: upperCase,
+        number: number,
+        specialChar: specialChar
+    };
+    return chosenChar;
+    
 }
 
-genPass();
-console.log(criteria);
-
-for (var i = 0; i < passLength; i++) {
-    var randomCriteria = Math.floor(Math.random() * criteria.length);
-    // Randomly pick from criteria array that already includes all criteria
-    password.push(criteria[randomCriteria]);
+// Get random characters from the array
+function getRandom(arr) {
+    var randomIndex = Math.floor(Math.random() * arr.length);
+    var randomChar = arr[randomIndex];
+    return randomChar;
 }
 
-console.log("Password: " + password.toString().replace(/,/g, ''));
+// Generate password from user input
+function genPass() {
+
+    var chosenChar = userInput();
+
+    // Possible array
+    possibleArray = [];
+
+    // Guaranteed array
+    guaranteedArray = [];
+
+    // Final result password
+    password = [];
+
+    // Join the chosen input in the possibleArray
+    // Push the random characters from the chosen character type array to the guaranteed array
+    if (chosenChar.lowerCase) {
+        possibleArray = possibleArray.concat(lowerCaseArray);
+        guaranteedArray.push(getRandom(lowerCaseArray));
+
+    }
+
+    if (chosenChar.upperCase) {
+        possibleArray = possibleArray.concat(upperCaseArray);
+        guaranteedArray.push(getRandom(upperCaseArray));
+
+    }
+
+    if (chosenChar.number) {
+        possibleArray = possibleArray.concat(numberArray);
+        guaranteedArray.push(getRandom(numberArray));
+
+    }
+
+    if (chosenChar.specialChar) {
+        possibleArray = possibleArray.concat(specialCharArray);
+        guaranteedArray.push(getRandom(specialCharArray));
+
+    }
+
+    // Iterate over the password length from the chosen characters in the possible array to the resulted password
+    for (var i = 0; i < chosenChar.passLength; i++) {
+        password.push(getRandom(possibleArray));
+    }
+    // Mix the guaranteed characters from the guaranteed array to the resulted password
+    for (var i = 0; i < guaranteedArray.length; i++) {
+        password[i] = guaranteedArray[i];
+    }
+
+    // Join the characters from the possible array and the guaranteed array to form the resulted password
+    return password.join("");
+}
+
+// Display the generated password in the card by clicking button
+
+var generateButton = document.querySelector("#generate");
+
+function displayPass() {
+    var password = genPass();
+    var passwordDisplay = document.querySelector("#password");
+    passwordDisplay.value = password;
+}
+
+generateButton.addEventListener("click", displayPass);
